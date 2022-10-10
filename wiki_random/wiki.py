d18ac001fa4b947d83fc056d8fd6d435e9009b61
Igor Moryto <56959606+Ezykill@users.noreply.github.com>
2022-10-10 14:15:58 +0200

    add Rick roll 1% chance

--- wiki_random/wiki.py
+++ wiki_random/wiki.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import random
 
 def get_wiki_random():
     """
@@ -8,11 +9,19 @@ def get_wiki_random():
     Returns:
         title and url
     """
-    url = requests.get("https://en.wikipedia.org/wiki/Special:Random")
-    soup = BeautifulSoup(url.content, "html.parser")
-    title = soup.find(class_="firstHeading").text
+    
+    ran = random.randrange(0,100)
+    
+    if ran == 1:
+        title = "Rick Roll"
+        url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
 
-    url = "https://en.wikipedia.org/wiki/%s" % title
+    else:
 
+        url = requests.get("https://en.wikipedia.org/wiki/Special:Random")
+        soup = BeautifulSoup(url.content, "html.parser")
+        title = soup.find(class_="firstHeading").text
+        url = "https://en.wikipedia.org/wiki/%s" % title
+    
     return title,url
     
